e32e6f8c6e1df03ef3c4dc67a231fb23d8dcd96b
Vladyslav Soloviov <ct.vladyslavsoloviov@waterford.org>
2025-08-28 02:35:59 +0300

    added .env

--- src/features/timers/api.ts
+++ src/features/timers/api.ts
@@ -1,21 +1,22 @@
 import { Timer, TimerResponse } from "./types"
+const URL = import.meta.env.VITE_API_URL
 
 export async function getTimer(id: string): Promise<TimerResponse> {
-  const response = await fetch(`http://localhost:8080/${id}`)
+  const response = await fetch(`${URL}/${id}`)
   const data: Timer = await response.json()
 
   return data
 }
 
 export async function getListOfTimers(): Promise<string[]> {
-  const response = await fetch("http://localhost:8080/list")
+  const response = await fetch(`${URL}/list`)
   const data: string[] = await response.json()
 
   return data
 }
 
 export async function postTimer(id: string): Promise<TimerResponse> {
-  const response = await fetch(`http://localhost:8080/reset/${id}`, {
+  const response = await fetch(`${URL}/reset/${id}`, {
     method: "POST",
   })
   const data: Timer = await response.json()
