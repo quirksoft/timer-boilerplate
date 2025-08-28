266b09b501704f5d9418a5e790bb564dd2a23915
Vladyslav Soloviov <ct.vladyslavsoloviov@waterford.org>
2025-08-28 22:10:11 +0300

    removed comments

--- src/app/store.ts
+++ src/app/store.ts
@@ -17,9 +17,6 @@ export const store = configureStore({
     getDefaultMiddleware().concat(tabsViewMiddleware, timerMiddelware),
 })
 
-// Get the type of our store variable
 export type AppStore = typeof store
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore["getState"]>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = AppStore["dispatch"]
